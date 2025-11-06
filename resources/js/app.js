51e3431b553a517a46be63b27960a50a44cf1645
rogz-github <rogz.nunez2013@gmail.com>
2025-11-07 01:00:37 +0800

    Changes 110725 - 1:01am

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,12 +7,14 @@ import App from './App.vue';
 import Home from './pages/Home.vue';
 import Advertising from './pages/Advertising.vue';
 import NewbizOrganic from './pages/NewbizOrganic.vue';
+import Newbiz from './pages/Newbiz.vue';
 import ApplicationForm from './pages/ApplicationForm.vue';
 
 const routes = [
     { path: '/', component: Home },
     { path: '/advertising', component: Advertising },
     { path: '/newbiz-organic', component: NewbizOrganic },
+    { path: '/newbiz', component: Newbiz },
     { path: '/application-form', component: ApplicationForm },
 ];
 
